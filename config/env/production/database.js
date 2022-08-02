c2af384c54d5338c6e507cdbaab0285f8165c55c
pr <pjr94@users.noreply.github.com>
2022-08-02 17:35:21 +0100

    update db

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
     connection: {
       client: 'postgres',
       connection: {
-        host: env('PGHOST', '127.0.0.1'),
-        port: env.int('PGPORT', 5931),
-        database: env('PGDATABASE', 'railway'),
+        host: env('PGHOST', 'db.gzjqytpxxwxlmxppdhxz.supabase.co'),
+        port: env.int('PGPORT', 5432),
+        database: env('PGDATABASE', 'postgres'),
         user: env('PGUSER', 'postgres'),
-        password: env('PGPASSWORD', 'password'),
+        password: env('PGPASSWORD', '58CM!4PR3Qpfcmh'),
         ssl: env.bool(true),
       },
     },
